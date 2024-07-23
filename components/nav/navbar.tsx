d3000adf2e0d6aada732db4bf955ab1261c0ce37
zhaoqi li <zli484@wisc.edu>
2024-07-22 19:31:05 -0700

    update logo

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -9,6 +9,8 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "@/images/logos/temp_logo.png";
+import Image from "next/image";
 
 const navigation = [
   { name: "Home", href: "/", current: false },
@@ -42,11 +44,7 @@ export default function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <Image className="h-8 w-auto" src={logo} alt="Your Company" />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
